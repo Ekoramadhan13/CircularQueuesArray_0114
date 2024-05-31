5a362ce48b8a4f3f2c1c44d3e0429654aa0022be
Ekoramadhan13 <enugraha411@gmail.com>
2024-05-31 10:29:41 +0700

    menambahkan class queues

--- algoritma3105/algoritma3105.cpp
+++ algoritma3105/algoritma3105.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class queues {
+    int front, rear, max = 5;
+    int queue_array[5];
 
+public:
+    queues() {
+        front = -1;
+        rear = -1;
+    }
+
+};
 int main()
 {
     
